f706f56b7ca9b08a1e45df6951fc9d0be41a579c
Serena-Dong <serenadong3159@icloud.com>
2023-06-14 23:09:35 +0200

    jumbotron

---
+++ src/components/contents/Jumbotron.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'Jumbotron'
+}
+</script>
+<template>
+    <div class="jumbotron">
+        <div class="container">
+            <div class="row align-items-end">
+                <div class="col-4 update">
+                    <p>Last updated 14-06-2023</p>
+                </div>
+                <div class="col-8 title">
+                    <h1>Full Stack Web Developer currently based in <b><u>Milan</u></b></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+.jumbotron {
+
+    .title h1 {
+        font-size: 5rem;
+    }
+}
+</style>
